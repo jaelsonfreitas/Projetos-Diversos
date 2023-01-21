f866cfde63aad8a53072c4597bfb5cd04b9d0201
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-01-21 09:41:30 -0300

    criando variavel button e adicionando tag addevent.

--- nlw_Setup/script.js
+++ nlw_Setup/script.js
@@ -1,11 +1,14 @@
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
 
-const data = {
-  run: ["01-01", "01-02", "01-06", "01-07", "01-08"],
-  takePills: ["01-03"],
-  journal: ["01-02"],
-}
+button. addEventListener()
 
-nlwSetup.setData(data)
-nlwSetup.load()
+// const data = {
+//   run: ["01-01", "01-02", "01-06", "01-07", "01-08"],
+//   takePills: ["01-03"],
+//   journal: ["01-02"],
+// }
+
+// nlwSetup.setData(data)
+// nlwSetup.load()
