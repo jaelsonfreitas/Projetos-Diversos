ae775cc29aa6beaabd96f7e4d85a97b66f8775f3
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-01-21 09:50:42 -0300

    adicionando configuraçoes basicas.

--- nlw_Setup/script.js
+++ nlw_Setup/script.js
@@ -2,7 +2,17 @@ const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
 const button = document.querySelector('header button')
 
-button. addEventListener()
+button. addEventListener('click', add)
+
+function add(params) {
+ 
+  const today = "01/01"
+  const dayExists = nlwSetup.dayExists(today)
+
+  alert(dayExists)
+
+  nlwSetup.addDay(today)
+}
 
 // const data = {
 //   run: ["01-01", "01-02", "01-06", "01-07", "01-08"],
