2a84e9aad087215906d977613e7ecfa879c95f88
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-01-21 21:31:31 -0300

    adicionando JSON.

--- nlw_Setup/script.js
+++ nlw_Setup/script.js
@@ -3,6 +3,7 @@ const nlwSetup = new NLWSetup(form)
 const button = document.querySelector('header button')
 
 button. addEventListener('click', add)
+form.addEventListener("change", save)
 
 function add() {
   const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
@@ -16,11 +17,18 @@ function add() {
   nlwSetup.addDay(today)
 }
 
+function save() {
+  localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+  
+}
+
+
 // const data = {
 //   run: ["01-01", "01-02", "01-06", "01-07", "01-08"],
 //   takePills: ["01-03"],
 //   journal: ["01-02"],
 // }
 
-// nlwSetup.setData(data)
-// nlwSetup.load()
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
+nlwSetup.setData(data)
+nlwSetup.load()
